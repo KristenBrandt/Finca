37e984431d158230431e19744b283e05df03ed89
KristenBrandt <kabrandtf@gmail.com>
2020-08-30 16:29:35 -0600

    Centros de Costo

--- FincApp/screens/cc.js
+++ FincApp/screens/cc.js
@@ -1,13 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text , View} from 'react-native';
-import {StatusBar} from "expo-status-bar";
+import React, {useState} from 'react';
+import {StyleSheet, Text, TextInput, View, TouchableOpacity, Image} from 'react-native';
+import back from "../assets/back.png";
 
 
 const cc =({navigation})=> {
+    const [Gasto, setGasto] = useState('');
+    const [Nombre,setNombre]= useState('');
+    const [Descripcion,setDescripcion]= useState('');
+    const [CentrodeCosto,setCentroCosto] =useState('');
+
+
     return (
         <View style={styles.container} >
-            <StatusBar style="auto" />
-            <Text style={{color:"'#fefae0'"}}>En construcción ....</Text>
+            <TouchableOpacity onPress={()=>navigation.navigate('home')}>
+                <Image source ={back} style ={{width:50, height:50,paddingHorizontal:5,marginBottom:50}}/>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <TextInput style = {styles.input}
+                           placeholder="Gasto"
+                           onChange = {(gas)=>setGasto(gas)}
+                />
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <TextInput style = {styles.input}
+                           placeholder="Nombre de Objeto"
+                           onChange = {(obj)=>setNombre(obj)}
+                />
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <TextInput style = {styles.input}
+                           placeholder="Descripcion"
+                           onChange = {(desc)=>setDescripcion(desc)}
+                />
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <TextInput style = {styles.input}
+                           placeholder="Centro de Costo"
+                           onChange = {(centro)=>setCentroCosto(centro)}
+                />
+            </TouchableOpacity>
+            < TouchableOpacity>
+                <Text style={styles.button}  >Ingresar datos</Text>
+            </TouchableOpacity>
+
         </View>
     )
 }
@@ -17,7 +52,37 @@ const styles = StyleSheet.create({
         backgroundColor: '#283618',
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    button: {
+        alignItems: 'center',
+        margin: 25,
+        paddingHorizontal: 30,
+        paddingTop: 20,
+        borderRadius: 50,
+        height: 70,
+        width: 270,
+        borderColor: 'black',
+        fontSize: 25,
+        color: '#fefae0',
+        backgroundColor: '#606C38',
+    },
+    input:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 5,
+        margin: 20,
+        borderRadius: 50,
+        height: 70,
+        width: 270,
+        borderColor: 'black',
+        fontSize: 20,
+        backgroundColor: '#fefae0'
+    },
+    text: {
+        color: '#fefae0',
+        fontSize: 15
+
     }
-})
+});
 
 export default cc
