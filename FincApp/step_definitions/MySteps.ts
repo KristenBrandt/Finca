f4431690e2bd5e629c171671a59e64ac57a1fa9b
KristenBrandt <kabrandtf@gmail.com>
2020-08-30 23:11:39 -0600

    BDD Gherkin

---
+++ FincApp/step_definitions/MySteps.ts
@@ -0,0 +1,16 @@
+import {Given} from 'cucumber'
+import {When} from 'cucumber'
+import {Then} from 'cucumber'
+
+Given(/^el administrador hizo una nueva compra$/, function () {
+
+});
+Given(/^ya esta registrado y logged in a el app$/, function () {
+
+});
+When(/^el administrador ingresa el costo, el nombre del articulo o servicio comprado, una pequeña descripcion y a que centro de costo pertenece$/, function () {
+
+});
+Then(/^el dato se ingresa a la base de datos si se encuentra online y si se encuentra offline se sube despues$/, function () {
+
+});
