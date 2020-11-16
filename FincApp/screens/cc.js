c7817a17f14862c34229cba13c31f94e6b550c1f
KristenBrandt <kabrandtf@gmail.com>
2020-11-15 20:51:44 -0600

    Centro de costos

--- FincApp/screens/cc.js
+++ FincApp/screens/cc.js
@@ -1,58 +1,117 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, TextInput, View, TouchableOpacity, Image} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, TextInput, View, TouchableOpacity, Image, Keyboard} from 'react-native';
 import back from "../assets/back.png";
 import { firebase } from '../Firebase';
+import {ScrollView} from "react-native-web";
 
 
-const cc =({navigation})=> {
+export default function ventas({navigation}) {
+    const entityRef = firebase.firestore().collection('ventas')
+
     const [Gasto, setGasto] = useState('');
     const [Nombre,setNombre]= useState('');
     const [Descripcion,setDescripcion]= useState('');
     const [CentrodeCosto,setCentroCosto] =useState('');
 
+    useEffect(() => {
+        entityRef
+            .orderBy('createdAt', 'desc')
+            .onSnapshot(
+                querySnapshot => {
+                    const newEntities = []
+                    querySnapshot.forEach(doc => {
+                        const entity = doc.data()
+                        entity.id = doc.id
+                        newEntities.push(entity)
+                    });
+                    setEntities(newEntities)
+                },
+                error => {
+                    console.log(error)
+                }
+            )
+    }, [])
+
+    const onAddButtonPress2 = () => {
+        if ((Gasto && Gasto.length) > 0 && (Nombre && Nombre.lenght) >0) {
+            const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+            const data = {
+                Gasto_Quetzales: Gasto,
+                Nombre: Nombre,
+                Descripcion: Descripcion,
+                CentrodeCosto_Asignado: CentrodeCosto,
+                creadoEn: timestamp,
+            };
+            //revisar ese alert @Todo
+            entityRef
+                .add(data)
+                .then(_doc => {
+                    data
+                    Keyboard.dismiss()
+                    alert("Costo Agregado")
+
+                })
+                .catch((error) => {
+                    alert(error)
+                });
+
+
+        }
+    }
+    const renderEntity = ({item, index}) => {
+        return (
+            <View style={styles.entityContainer}>
+                <Text style={styles.entityText}>
+                    {index}. {item.text}
+                </Text>
+            </View>
+        )
+    }
 
     return (
-        <View style={styles.container} >
-            <TouchableOpacity onPress={()=>navigation.navigate('menu')}>
-                <Image source ={back} style ={{width:50, height:50,paddingHorizontal:5,marginBottom:50}}/>
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <TextInput style = {styles.input}
-                           placeholder="Gasto"
-                           placeholderTextColor="#283618"
-                           value = {Gasto}
-                           onChange = {(gas)=>setGasto(gas)}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <TextInput style = {styles.input}
-                           placeholder="Nombre de Objeto"
-                           placeholderTextColor="#283618"
-                           value = {Nombre}
-                           onChange = {(obj)=>setNombre(obj)}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <TextInput style = {styles.input}
-                           placeholder="Descripcion"
-                           placeholderTextColor="#283618"
-                           value = {Descripcion}
-                           onChange = {(desc)=>setDescripcion(desc)}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <TextInput style = {styles.input}
-                           placeholder="Centro de Costo"
-                           placeholderTextColor="#283618"
-                           value = {CentrodeCosto}
-                           onChange = {(centro)=>setCentroCosto(centro)}
-                />
-            </TouchableOpacity>
-            < TouchableOpacity style={styles.button}>
-                <Text  style={{ fontSize: 22,color:'#fefae0'}} >Ingresar datos</Text>
-            </TouchableOpacity>
+        <ScrollView>
+            <View style={styles.container} >
+                <TouchableOpacity onPress={()=>navigation.navigate('menu')}>
+                    <Image source ={back} style ={{width:50, height:50,paddingHorizontal:5,marginBottom:50}}/>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <TextInput style = {styles.input}
+                               placeholder="Gasto"
+                               placeholderTextColor="#283618"
+                               value = {Gasto}
+                               onChange = {(gas)=>setGasto(gas)}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <TextInput style = {styles.input}
+                               placeholder="Nombre de Objeto"
+                               placeholderTextColor="#283618"
+                               value = {Nombre}
+                               onChange = {(obj)=>setNombre(obj)}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <TextInput style = {styles.input}
+                               placeholder="Descripcion"
+                               placeholderTextColor="#283618"
+                               value = {Descripcion}
+                               onChange = {(desc)=>setDescripcion(desc)}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <TextInput style = {styles.input}
+                               placeholder="Centro de Costo"
+                               placeholderTextColor="#283618"
+                               value = {CentrodeCosto}
+                               onChange = {(centro)=>setCentroCosto(centro)}
+                    />
+                </TouchableOpacity>
+                < TouchableOpacity style={styles.button} onPress ={() => onAddButtonPress2()}>
+                    <Text  style={{ fontSize: 22,color:'#fefae0'}} >Ingresar datos</Text>
+                </TouchableOpacity>
 
-        </View>
+            </View>
+        </ScrollView>
     )
 }
 const styles = StyleSheet.create({
